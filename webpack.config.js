8cd3f73ac2d21ec62e5e904f449faa0e19318741
Andrew Peterson <andy@ndpsoftware.com>
2016-09-30 22:23:02 -0700

    next version

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,12 @@ var config = {
   module  : {
     loaders : [
       {
-        test    : /(\.jsx|\.js)$/,
+        test    : /\.js$/,
         loader  : 'babel',
-        exclude : /(node_modules|bower_components)/
+        exclude : /node_modules/
       },
       {
-        test    : /(\.jsx|\.js)$/,
+        test    : /\.js$/,
         loader  : "eslint-loader",
         exclude : /node_modules/
       }
