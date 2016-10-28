d9a13610a0b49793ee9abd2dfc14209cef0d4407
Andrew Peterson <andy@ndpsoftware.com>
2016-10-27 23:14:25 -0700

    Normalize tests. Add tests around the functional usage of pantry factories.

--- src/test-pantry.spec.js
+++ src/test-pantry.spec.js
@@ -12,49 +12,52 @@ describe('pantry', function() {
 
   describe('definition: ', function() {
 
-    it('is defined using an object literal', function() {
+    it('a number', function() {
+      pantry.recipeFor('id', 42)
+      expect(pantry.id()).to.eql(42)
+    })
+
+    it('an object literal', function() {
       pantry.recipeFor('myObj', {key: 'value'})
       const result = pantry.myObj()
       expect(result).to.eql({key: 'value'})
     })
 
-    it('is defined using a function returning an object', function() {
+    it('a function returning an object', function() {
       pantry.recipeFor('myObj', () => ({key: 'value'}))
       const result = pantry.myObj()
       expect(result).to.eql({key: 'value'})
     })
 
-    it('is defined with function returning a string', function() {
+    it('a function returning a string', function() {
       pantry.recipeFor('id', function() {
         return `id-${this.count}`
       })
       expect(pantry.id()).to.eql('id-1')
     })
 
-    it('is defined with function returning a number', function() {
+    it('a function returning a number', function() {
       pantry.recipeFor('id', function() {
         return this.count
       })
       expect(pantry.id()).to.eql(1)
     })
 
-    it('can be defined with an afterCreate function', function() {
-      const genName = () => 'foo'
-      pantry.recipeFor('person',
-                       function() {
-                         return {first: genName(), last: 'last'}
-                       },
-                       function(o) {
-                         o.fullName = `${o.first} ${o.last}`;
-                         return o
-                       })
-
-      const result = pantry.person()
-      expect(Object.keys(result)).to.eql(['first', 'last', 'fullName'])
-      expect(result.fullName).to.eql('foo last')
+    it('an arbitrary string isn\'t allowed', function() {
+      expect(() => pantry.recipeFor('id', 'id-1')).to.throw(/id\-1.*is not a known factory name/)
     })
 
-    it('can be built from other factories', function() {
+    it('another factory name', function() {
+      pantry.recipeFor('a', {a: 'a'})
+      pantry.recipeFor('b', {b: 'b'})
+      pantry.recipeFor('c', {c: 'c'})
+
+      pantry.recipeFor('abc', 'a', 'b', 'c')
+
+      expect(pantry('abc')).to.eql({a: 'a', b: 'b', c: 'c'})
+    })
+
+    it('another factory', function() {
       pantry.recipeFor('a', function() {
         return {a: 'a'}
       })
@@ -64,18 +67,60 @@ describe('pantry', function() {
       pantry.recipeFor('c', function() {
         return {c: 'c'}
       })
-      pantry.recipeFor('abc', 'a', 'b', 'c')
+
+      pantry.recipeFor('abc', pantry.a, pantry.b, pantry.c)
 
       expect(pantry('abc')).to.eql({a: 'a', b: 'b', c: 'c'})
-
     })
 
-    it('factory function can access context', function() {
+    it('an afterCreate function using first values', function() {
+      pantry.recipeFor('person',
+                       {first: 'foo', last: 'last'},
+                       function(o) {
+                         o.fullName = `${o.first} ${o.last}`;
+                         return o
+                       })
+
+      const result = pantry.person()
+
+      expect(Object.keys(result)).to.eql(['first', 'last', 'fullName'])
+      expect(result.fullName).to.eql('foo last')
+    })
+
+    it('an afterCreateFn can access context', function() {
+      pantry.recipeFor('myObj', {key: 'value'}, function(x) {
+        return Object.assign({}, x, {key1: `${x.key}${this.count}`})
+      })
+
+      const result = pantry.myObj()
+
+      expect(result).to.eql({key: 'value', key1: 'value1'})
+    })
+
+    it('can use `count`', function() {
       pantry.recipeFor('myObj', function() {
         return {key: `value-${this.count}`}
       })
       expect(pantry.myObj()).to.eql({key: 'value-1'})
       expect(pantry.myObj()).to.eql({key: 'value-2'})
+    })
+
+    it('can use the factory\'s `name`', function() {
+      pantry.recipeFor('dog', function() {
+        return {key: this.name}
+      })
+      expect(pantry.dog()).to.eql({key: 'dog'})
+    })
+
+    it('can use the factory\'s `name` in different factories', function() {
+      pantry.recipeFor('hasId', function() {
+        return {id: `${this.name}-${this.count}`}
+      })
+      pantry.recipeFor('dog', {bark: 'yes'}, 'hasId')
+      pantry.recipeFor('cat', {meow: 'yes'}, 'hasId')
+
+      expect(pantry.dog()).to.eql({id: 'dog-1', bark: 'yes'})
+      expect(pantry.cat()).to.eql({id: 'cat-1', meow: 'yes'})
     })
 
     it('can restart counts', function() {
@@ -95,19 +140,16 @@ describe('pantry', function() {
       expect(pantry.count()).to.eql(3)
     })
 
-    it('afterCreateFn can access context', function() {
-      pantry.recipeFor('myObj', {key: 'value'}, function(x) {
-        return Object.assign({}, x, {key1: `${x.key}${this.count}`})
-      })
+    it('recipeFor() returns the factory method', function() {
+      const f = pantry.recipeFor('myObj', {key: 'value'})
 
-      const result = pantry.myObj()
-      expect(result).to.eql({key: 'value', key1: 'value1'})
+      const result = f()
+
+      expect(result).to.eql({key: 'value'})
     })
 
-    it('recipeFor() returns the factory method', function() {
-      const f      = pantry.recipeFor('myObj', {key: 'value'})
-      const result = f()
-      expect(result).to.eql({key: 'value'})
+    it('recipeFor() returns the factory method that accepts a count', function() {
+      const f = pantry.recipeFor('myObj', {key: 'value'})
 
       expect(f(1)).to.eql([{key: 'value'}])
     })
@@ -194,12 +236,7 @@ describe('pantry', function() {
 
   })
 
-  describe('cooking: ', function() {
-    it('will return value directly from pantry function', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = pantry('myObj')
-      expect(result).to.eql({key: 'value'})
-    })
+  describe('now we\'re cooking: ', function() {
 
     it('handles several different object-based factories', function() {
       pantry.recipeFor('id', {
@@ -260,7 +297,8 @@ describe('pantry', function() {
       expect(r).to.eql({id: 1, name: 'name #1', timestamp: 'june 4th'})
     })
 
-    describe('recipe does not accept arguments', function() {
+    describe('when recipe does not accept arguments', function() {
+
       it('merges params given', function() {
         pantry.recipeFor('x', function() {
           return {x: 5}
@@ -270,7 +308,8 @@ describe('pantry', function() {
       })
     })
 
-    describe('recipe accepts arguments', function() {
+    describe('when recipe accepts arguments', function() {
+
       it('passes the first parameter in to first function', function() {
         pantry.recipeFor('x', function(inputs) {
           return {x: 3 * inputs.y}
@@ -280,78 +319,72 @@ describe('pantry', function() {
       })
     })
 
-    it('will create multiple objects when a number is given', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = pantry(2, 'myObj')
-      expect(result).to.eql([{key: 'value'}, {key: 'value'}])
-    })
-
-    it('will create multiple objects specific recipe is given a number', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = pantry.myObj(2)
-      expect(result).to.eql([{key: 'value'}, {key: 'value'}])
-    })
-
-    it('can be used in a filled array loop', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = Array(2).fill().map(pantry.myObj)
-      expect(result).to.eql([{key: 'value'}, {key: 'value'}])
-    })
-
-    it('can be used in a `map` loop', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = [1, 3, 5].map(x => ({id: `id-${x}`})).map(pantry.myObj)
-      expect(result).to.eql([
-        {id: 'id-1', key: 'value'},
-        {id: 'id-3', key: 'value'},
-        {id: 'id-5', key: 'value'}
-      ])
-    })
-
-    it('can be with spread operator', function() {
-      pantry.recipeFor('myObj', {key: 'value'})
-      const result = [...Array(2)].map(pantry.myObj)
-      expect(result).to.eql([{key: 'value'}, {key: 'value'}])
-    })
-
     it('an "after" function can access factory object', function() {
       pantry.recipeFor('cat', function() {
         return {id: `id-${this.count}`}
       })
 
-      const cats = pantry.cat(2, function(o) {
+      const cat = pantry.cat(function(o) {
         o.id = `cat-${o.id}`;
         return o
       })
 
-      expect(cats).to.eql([
-        {
-          'id': 'cat-id-1'
-        },
-        {
-          'id': 'cat-id-2'
-        }
-      ])
+      expect(cat).to.eql({
+        'id': 'cat-id-1'
+      })
     })
 
-    xit('an "after" function can access `this` values', function() {
+    it('an "after" function can access `this` values', function() {
       pantry.recipeFor('cat', function() {
         return {id: `id-${this.count}`}
       })
 
-      const cats = pantry.cat(2, function(o) {
+      const cat = pantry.cat(function(o) {
         o.id = `cat-id-${this.count}`;
         return o
       })
 
-      expect(cats).to.eql([
-        {
-          'id': 'cat-id-1'
-        },
-        {
-          'id': 'cat-id-2'
-        }
-      ])
+      expect(cat).to.eql({
+        'id': 'cat-id-1'
+      })
+    })
+
+    describe('multiple objs', function() {
+
+      it('will create multiple objects when a number is given', function() {
+        pantry.recipeFor('myObj', {key: 'value'})
+        const result = pantry(2, 'myObj')
+        expect(result).to.eql([{key: 'value'}, {key: 'value'}])
+      })
+
+      it('will create multiple objects specific recipe is given a number', function() {
+        pantry.recipeFor('myObj', {key: 'value'})
+        const result = pantry.myObj(2)
+        expect(result).to.eql([{key: 'value'}, {key: 'value'}])
+      })
+
+      it('can be used in a filled array loop', function() {
+        pantry.recipeFor('myObj', {key: 'value'})
+        const result = Array(2).fill().map(pantry.myObj)
+        expect(result).to.eql([{key: 'value'}, {key: 'value'}])
+      })
+
+      it('can be used in a `map` loop', function() {
+        pantry.recipeFor('myObj', {key: 'value'})
+        const result = [1, 3, 5].map(x => ({id: `id-${x}`})).map(pantry.myObj)
+        expect(result).to.eql([
+          {id: 'id-1', key: 'value'},
+          {id: 'id-3', key: 'value'},
+          {id: 'id-5', key: 'value'}
+        ])
+      })
+
+      it('can be with spread operator', function() {
+        pantry.recipeFor('myObj', {key: 'value'})
+        const result = [...Array(2)].map(pantry.myObj)
+        expect(result).to.eql([{key: 'value'}, {key: 'value'}])
+      })
+
     })
 
   })
